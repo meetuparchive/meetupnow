2e35d4b6032c917afee3c4f56cd3d91542a3442a
Michael Shapiro <michael@michael.dev.meetup.com>
2010-06-14 16:35:31 -0400

    updated UserActions.java

--- src/meetupnow/UserActions.java
+++ src/meetupnow/UserActions.java
@@ -3,25 +3,76 @@ package meetupnow;
 import com.google.appengine.api.datastore.Key;
 
 import java.io.IOException;
+import java.util.Properties;
+import java.util.StringTokenizer;
+import java.util.List;
 import javax.servlet.http.*;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Transaction;
 import javax.jdo.Query;
-import java.util.Properties;
-import java.util.StringTokenizer;
-import java.util.List;
 
 import org.scribe.oauth.*;
-
+import org.scribe.http.*;
 import org.apache.commons.codec.*;
+import org.json.*;
 
 import meetupnow.MeetupUser;
 import meetupnow.PMF;
 
-import javax.servlet.http.Cookie;
 
 public class UserActions extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String API_URL = "http://api.meetup.com/ew/events/?urlname=MUNTest&zip=10012&venuename=somewhere";
+		String key = "empty";
+    		javax.servlet.http.Cookie[] cookies = req.getCookies();
+    		if (cookies != null) {
+      			for (int i = 0; i < cookies.length; i++) {
+        			if (cookies[i].getName().equals("meetup_access")) {
+          				key = cookies[i].getValue();
+        			}
+      			}
+    		}
+		if (key.equals("empty")) {
+			resp.sendRedirect("/MeetupNow");
+		}	
 		
+		Properties prop = new Properties();
+		prop.setProperty("consumer.key","12345");
+		prop.setProperty("consumer.secret","67890");
+		Scribe scribe = new Scribe(prop);
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Query query = pm.newQuery(MeetupUser.class);
+		query.setFilter("accToken == accTokenParam");
+		query.declareParameters("String accTokenParam");
+
+		try {
+			List<MeetupUser> users = (List<MeetupUser>) query.execute(key);
+			if (users.iterator().hasNext()) {
+				Token accessToken = new Token(users.get(0).getAccToken(),users.get(0).getAccTokenSecret());
+				Request APIrequest = new Request(Request.Verb.POST, API_URL);
+				scribe.signRequest(APIrequest,accessToken);
+				Response APIresponse = APIrequest.send();
+				JSONObject json = new JSONObject();
+				try {
+					json = new JSONObject(APIresponse.getBody());
+					
+					resp.getWriter().println(json.toString());
+					String[] names = JSONObject.getNames(json.getJSONArray("results").getJSONObject(0));
+					for (int j = 0; j < json.getJSONArray("results").length(); j++) {
+					
+						for (int i = 0; i < names.length; i++) {
+							String temp = json.getJSONArray("results").getJSONObject(j).getString(names[i]);
+							resp.getWriter().println(temp);
+						}	
+					}
+				} catch (JSONException j) {
+		
+				}
+			}
+		}
+		finally {
+			query.closeAll();
+		}
+
 	}
 }
