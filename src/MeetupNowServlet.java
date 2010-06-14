7076b4156f90d0c473f1b82f0af04c38f6b3f547
Michael Shapiro <michael@michael.dev.meetup.com>
2010-06-14 15:08:10 -0400

    minor changes

--- src/MeetupNowServlet.java
+++ src/MeetupNowServlet.java
@@ -6,6 +6,18 @@ import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
+import java.io.IOException;
+import javax.servlet.http.*;
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import java.util.Properties;
+import java.util.StringTokenizer;
+import java.util.List;
+
+import org.scribe.oauth.*;
+
+import org.apache.commons.codec.*;
+import MeetupNow.PMF;
 
 public class MeetupNowServlet extends HttpServlet {
      public void doGet(HttpServletRequest req, HttpServletResponse resp)
